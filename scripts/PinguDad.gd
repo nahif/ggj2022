b38bd2ed31ab073b87190a2df0dce396afc07d0a
nahif <ifferreira@uc.cl>
2022-01-29 00:20:32 -0300

    add dad pinguin scene

--- scripts/PinguDad.gd
+++ scripts/PinguDad.gd
@@ -1,16 +1,29 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var velocity = 500
+var min_velocity = 0.01
+var inertia = 9
+var vel : Vector2
+var constant_velocity = Vector2.LEFT * 60
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _input(event):
+	if event.is_action_pressed("ui_right"):
+		_add_velocity(Vector2.RIGHT)
+	if event.is_action_pressed("ui_left"):
+		_add_velocity(Vector2.LEFT)
+
+func _add_velocity(direction : Vector2):
+	vel = direction * velocity
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	$Sprite.position = $Sprite.position + constant_velocity * delta
+	if vel.length() > min_velocity:
+		$Sprite.position = $Sprite.position + vel * delta
+		vel = vel - (vel * inertia * delta)
+	pass
---
+++ scripts/PinguinWall.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var constant_velocity = Vector2.LEFT * 60
+var velocity = 600
+var inertia = 9
+var timer_init = 0.9
+var vel : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var timer = Timer.new()
+	timer.autostart = true
+	timer.start(1)
+	timer.connect("timeout", self, "_jump")
+	add_child(timer)
+	pass # Replace with function body.
+
+func _jump():
+	vel = Vector2.RIGHT * velocity
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position = position + constant_velocity * delta + vel * delta
+	if vel.length() > 0:
+		vel = vel - (vel * inertia * delta)
+	pass
